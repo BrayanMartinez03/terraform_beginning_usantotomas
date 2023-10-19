54502ea37ed765ce0b03613727f54bf6d9cf8dd4
bmart10 <bmart10@bancodebogota.com.co>
2023-10-18 20:48:46 -0500

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # terraform_beginning_usantotomas
-curso for university santo tomas about terraform 
+
+### You have to install python3 
+    - Go to the official Python website at https://www.python.org/downloads/
+    - Click on the "Download Python 3.x.x" button (where x.x is the latest version)
+    - Choose the appropriate version for your operating system (Windows, macOS, or Linux)
+    - Download the installer and run it
+    - Follow the instructions in the installer to complete the installation
+    
+### You have to install pip3 
+    - Download the get-pip.py file from https://bootstrap.pypa.io/get-pip.py
+    - Open a terminal and navigate to the directory where the get-pip.py file was downloaded
+    - Run the command "python3 get-pip.py" (without quotes)
+    - Follow the instructions in the installer to complete the installation
+
+
+### You have to insrtall Terraform and Tfswitch 
+
+    TERRAFORM 
+       -  Go to the official Terraform website at https://www.terraform.io/downloads.html
+       - Download the appropriate package for your operating system (Windows, macOS, or Linux)
+       - Extract the downloaded package to a directory on your system
+       - Add the directory to your system's PATH environment variable
+
+       If you can't install terraform, please see this manual https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+
+    Tfswitch
+        - Go to the official tfswitch GitHub repository at https://github.com/warrensbox/terraform-switcher
+        - Download the appropriate package for your operating system (Windows, macOS, or Linux)
+        - Extract the downloaded package to a directory on your system
+        - Add the directory to your system's PATH environment variable
+
+
+### You have to install Docker 
+    - Go to the official Docker website at https://www.docker.com/products/docker-desktop
+    - Download the appropriate package for your operating system (Windows or macOS)
+    - Run the installer and follow the instructions to complete the installation
+    - Once the installation is complete, open Docker Desktop
+
---
+++ terraform/main.tf
@@ -0,0 +1,135 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_kms_key" "this" {
+  description             = "kms using in universidad santo tomas project"
+  deletion_window_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = "/aws/ecs/universidad-sato-tomas-app-python"
+}
+
+resource "aws_ecs_cluster" "this" {
+  name = "universidad-sato-tomas-app-python"
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.this.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.this.name
+      }
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "example" {
+  cluster_name = aws_ecs_cluster.this.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
+
+resource "aws_ecs_task_definition" "test" {
+  family                   = "app-python"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = "arn:aws:iam::636400435876:role/ecsTaskExecutionRole"
+
+  container_definitions = templatefile("${path.module}/container_definitions.json",
+    {
+      account_id     = "636400435876"
+      region         = "us-east-2"
+      container_name = "universidad-sato-tomas-app-python"
+      container_port = 5000
+      ecr_name       = aws_ecr_repository.this.id
+    }
+  )
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+resource "aws_ecs_service" "this" {
+  name            = "app-python"
+  cluster         = aws_ecs_cluster.this.name
+  task_definition = aws_ecs_task_definition.test.arn
+  desired_count   = 1
+  network_configuration {
+    subnets          = ["subnet-018df030d18797eee", "subnet-04b983dec4193a880", "subnet-01d2c38a9a9a74958"]
+    security_groups  = ["sg-0c73df152db741129"]
+    assign_public_ip = false
+  }
+
+  capacity_provider_strategy {
+    base              = 1
+    capacity_provider = "FARGATE"
+    weight            = 100
+  }
+
+  deployment_circuit_breaker {
+    enable   = false
+    rollback = false
+  }
+
+  load_balancer {
+   target_group_arn = element(concat(module.alb.target_group_arns, []), 0)
+   container_name   = "universidad-sato-tomas-app-python"
+   container_port   = 5000
+ }
+}
+
+resource "aws_ecr_repository" "this" {
+  name                 = "usantotomas-app-python"
+  image_tag_mutability = "MUTABLE"
+}
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "usantotomas-application-lb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = "vpc-047f6b1c67309dae6"
+  subnets         = ["subnet-018df030d18797eee", "subnet-04b983dec4193a880", "subnet-01d2c38a9a9a74958"]
+  security_groups = ["sg-0c73df152db741129"]
+
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 5000
+      target_type      = "ip"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 5000
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
+
